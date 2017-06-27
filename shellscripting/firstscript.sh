079c8a40bc6686efbb1c37667b5251ff7726bc3d
KEETS <KEETS@MYCOMPANY.COM>
2017-06-27 11:05:26 +0530

    started practicing shell scripting

---
+++ shellscripting/firstscript.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+###############################
+#Purpose  : This is my first shell script program
+#Version  : 1.0
+#Owner	  : tk@keerthinag.com
+#Input 	  : None
+#Output   : to display a message as an output
+###############################
+
+#clearing out the current screen
+clear
+
+#Displaying user message
+echo "Welcome to Shell Scripting first program !!!"
+
+#Do nothing for 10 seconds 
+sleep 10 
+
+echo "-----END-----" #Ending the script with this message
+
---
+++ shellscripting/template.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+###############################
+#Purpose  : This is the template to be used
+#Version  : 1.0
+#Owner	  : tk@keerthinag.com
+#Input 	  : 
+#Output   : 
+###############################
