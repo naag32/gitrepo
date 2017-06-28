1a64005d446faeb58df9a09a35e431bd042c3182
KEETS <KEETS@MYCOMPANY.COM>
2017-06-28 09:11:46 +0530

    Learned variables in Shellscripting file variables.sh

---
+++ shellscripting/variables.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+###############################
+#Purpose  : This is to define variables
+#Version  : 1.0
+#Owner	  : tk@keerthinag.com
+#Input 	  : none
+#Output   : none
+###############################
+
+#Clearing the screen
+clear
+
+#Reading the variable value from command line
+echo " The value of COMPNAME from command line is $COMPNAME"
+
+#Reading the variable value from the shell script
+COMPNAME=ibm
+echo " The value of COMPNAME from shell script is $COMPNAME"
+
+#Reading the variable value as user defined ones
+echo "Please enter a value for COMPNAME:"
+read COMPNAME
+echo " The value of COMPNAME as defined by user is $COMPNAME"
+
+#Learning the difference between '(SingleQuote),"(DoubleQuote) and `(Backtick)
+COMMAND1=date
+echo "The value of \$COMMAND1 in SingleQuote is \$COMMAND1"
+echo "The value of \$COMMAND1 in DoubleQuote is $COMMAND1"
+echo "The value of \$COMMAND1 in BackTick is `$COMMAND1`"
+
+
+
